53e08e09bbc88f9eacf3d04fd1bdf0f35052a472
darklinkzito <andre_luiz_7777@hotmail.com>
2022-05-21 10:40:57 -0300

    Css Inline

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import './App.css';
+import MyComponent from './components/MyComponent';
+import {useState} from 'react';
 
 function App() {
+  const n = 15;
+  const [name] = useState("André");
   return (
-    <div>
+    <div className="App">
+    {/* CSS Global */}
       <h1>React com CSS</h1>
+      {/* CSS Component */}
+      <MyComponent />
+      <p>Este paragrafo é do App.js</p>
+      {/* Inline CSS */}
+      <p style={{color: "blue", padding: "25px", borderTop: "2px solid red"}}>
+        Este elemento foi estilizado de forma inline
+      </p>
+      {/* CSS inline dinâmico */}
+      <h2 style={n < 10 ? ({color: "purple"}) : ({color: "pink"})}>CSS Dinâmico</h2>
+      <h2 style={n > 10 ? ({color: "purple"}) : ({color: "pink"})}>CSS Dinâmico</h2>
+      <h2 style={name == "André" ? ({color: "gold", backgroundColor: "green"}) : ({color: "pink"})}>André</h2>
     </div>
   );
 }
---
+++ src/components/MyComponent.js
@@ -0,0 +1,13 @@
+import "./MyComponent.css";
+
+const MyComponent = () => {
+  return (
+    <div className="App">
+        <h1>Componente com CSS</h1>
+        <p>Paragrafo com css</p>
+        <p className="my-comp-paragraph">esse tb é do component</p>
+    </div>
+  )
+}
+
+export default MyComponent
